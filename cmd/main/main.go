7d6a68d8088cf3e1f8d1e7701a2bb70606a4a5ed
northwindman <northwindman@mail.ru>
2024-06-11 13:19:14 +0300

    The first relization of project

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/northwindman/GoAndMYsql/pkg/routes"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":9010", r))
+}
